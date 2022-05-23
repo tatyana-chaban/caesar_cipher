ef342f8094af185a0dcd3ba500a25c2bde7020a5
Tatyana Chaban <tatyana.chaban12@gmail.com>
2022-05-24 00:07:47 +0300

    add brut force

--- src/java/com/javarush/tchaban/cryptoanalyser/Main.java
+++ src/java/com/javarush/tchaban/cryptoanalyser/Main.java
@@ -10,11 +10,11 @@ public class Main {
                 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'я', '.', ',', '«', '»',
                 ':', '!', '?', ' ');
         String pathToFileForCrypt = "C:\\Users\\Nimfadora\\IdeaProjects\\caesar_cipher\\src\\forCrypt.txt";
-        int key = 2;
+        int key = 3;
 
-       // Scanner scanner = new Scanner(System.in);
-       // pathToFileForCrypt = scanner.nextLine();
-       // key = scanner.nextInt();
+        // Scanner scanner = new Scanner(System.in);
+        // pathToFileForCrypt = scanner.nextLine();
+        // key = scanner.nextInt();
 
 
         BufferedReader bufferCharFromFile = new BufferedReader(new FileReader(pathToFileForCrypt));
@@ -46,7 +46,7 @@ public class Main {
                 int position = ALPHABET.indexOf(oneChar);
                 int positionAfterDeCrypt = position - key;
                 if (positionAfterDeCrypt < 0) {
-                    positionAfterDeCrypt = ALPHABET.size()  + positionAfterDeCrypt;
+                    positionAfterDeCrypt = ALPHABET.size() + positionAfterDeCrypt;
                 }
                 bufferDecrypt.write(ALPHABET.get(positionAfterDeCrypt));
             } else {
@@ -57,6 +57,39 @@ public class Main {
         bufferDecrypt.close();
         bufferCrypt.close();
 
+//////////////////////////////
+        BufferedReader reader = new BufferedReader(new FileReader("C:\\Users\\Nimfadora\\IdeaProjects\\caesar_cipher\\Crypt.txt"));
+        BufferedWriter writer = new BufferedWriter(new FileWriter("C:\\Users\\Nimfadora\\IdeaProjects\\caesar_cipher\\DeCryptBrut.txt"));
 
+        StringBuilder stringBuilder = new StringBuilder();
+        while (reader.ready()) {
+            stringBuilder.append((char) reader.read());
+        }
+        for (int ourKey = 0; ourKey < ALPHABET.size(); ourKey++) {
+            StringBuilder stringBuilder2 = new StringBuilder();
+            for (int i = 0; i < stringBuilder.length(); i++) {
+                char oneChar = stringBuilder.charAt(i);
+                if (ALPHABET.contains(oneChar)) {
+                    int position = ALPHABET.indexOf(oneChar);
+                    int positionAfterDeCrypt = position - ourKey;
+                    if (positionAfterDeCrypt < 0) {
+                        positionAfterDeCrypt = ALPHABET.size() + positionAfterDeCrypt;
+                    }
+                    stringBuilder2.append(ALPHABET.get(positionAfterDeCrypt));
+                } else {
+                    stringBuilder2.append(oneChar);
+                }
+            }
+            String text = stringBuilder2.toString();
+            if (text.contains(". ") && text.contains(", ")) {
+                writer.write(text);
+                break;
+            }
+        }
+
+        reader.close();
+        writer.close();
     }
+
+
 }
